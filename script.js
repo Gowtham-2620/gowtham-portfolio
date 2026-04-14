c3544bc8e3d99aa1555b3e382794a370f2942b11
Gowtham <gowthamprabhu6884@gmail.com>
2026-04-14 20:19:59 +0530

    menu bar add

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('nav ul');
+
+if (hamburger && navbar) {
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navbar.classList.toggle('active');
+    });
+}
+
+const darkModeButton = document.getElementById('darkmodebutton');
+const body = document.body;
+
+if (darkModeButton) {
+    darkModeButton.addEventListener('change', () => {
+        if (darkModeButton.checked) {
+            body.classList.add('body.dark-mode');
+        } else {
+            body.classList.remove('body.dark-mode');
+        }
+    });
+}
 document.querySelectorAll('a[href^m"#"]').foreach(anchor =>{
     anchor.addEventListener('click',function (e){
         e.preventDefault();
